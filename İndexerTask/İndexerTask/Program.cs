2ed14c56823a27e984642230fba2495d9ede8ea0
MakirovicKH <mahirrx-ap215@code.edu.az>
2024-10-25 12:00:33 +0400

    INdexer Task

---
+++ İndexerTask/İndexerTask/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace İndexerTask
+{
+    internal class Program
+    {
+        static void Main(string[] city)
+        {
+
+            static void Main()
+            {
+                string[] indexOfCity = { "Baki", "NewYork", "İndoneziya", "İsmayilli" };
+                StringList cityList = new StringList(indexOfCity);
+
+                Console.WriteLine(cityList["Baki"]);       
+                Console.WriteLine(cityList["NewYork"]);   
+                Console.WriteLine(cityList["İndoneziya"]);      
+                Console.WriteLine(cityList["İsmayilli"]); 
+            }
+        }
+    }
+}
---
+++ İndexerTask/İndexerTask/StringList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İndexerTask
+{
+    internal class StringList
+    {
+
+        private string[] cities;
+
+        public StringList(string[] indexOfCity)
+        {
+            cities = indexOfCity;
+        }
+
+        // Indexer methoduu
+        public int this[string cityName]
+        {
+            get
+            {
+                for (int i = 0; i < cities.Length; i++)
+                {
+                    if (cities[i] == cityName)
+                    {
+                        return i;
+                    }
+                }
+                // sheher tapilmiir bele olanda asaguida
+                return -1;
+            }
+        }
+    }
+}
