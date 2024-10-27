ec424341695a23a8885cf2f5862e23e122601f3b
MakirovicKH <mahirrx-ap215@code.edu.az>
2024-10-27 21:47:30 +0400

    Reflection task

---
+++ ReflectionTask/ReflectionTask/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection;
+
+namespace ReflectionTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type userType = assembly.GetType("ReflectionExample.User"); 
+
+            // Obyekt yaratmaq üçün 'Activator.CreateInstance' dən istifadə etməliyik demeəli search edəndə öyrəndim bunu
+            object userInstance = Activator.CreateInstance(userType);
+
+            var fields = new (string name, object value)[]
+            {
+                ("id", 1),
+                ("name", "Mahir"),
+                ("soyad", "Xəlilov"),
+                ("age", 45) // 'age' statik sahədir, instance istəmir ona gorə .static yazılacaq
+            };
+
+            foreach (var field in fields)
+            {
+                FieldInfo fieldInfo = userType.GetField(field.name, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                fieldInfo?.SetValue(fieldInfo.IsStatic ? null : userInstance, field.value);
+            }
+
+            { }
+
+
+
+            foreach (var field in fields)
+            {
+                FieldInfo fieldInfo = userType.GetField(field.name, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                Console.WriteLine($"{field.name}: {fieldInfo?.GetValue(fieldInfo.IsStatic ? null : userInstance)}");
+            }
+
+//Method hissəsi
+            userType.GetMethod("GetFullName", BindingFlags.Public | BindingFlags.Instance)?.Invoke(userInstance, null);
+            userType.GetMethod("ChangeAge", BindingFlags.Public | BindingFlags.Static)?.Invoke(null, new object[] { 30 });
+
+            
+            FieldInfo ageField = userType.GetField("age", BindingFlags.NonPublic | BindingFlags.Static);
+            Console.WriteLine("New age: " + ageField?.GetValue(null));
+        }
+
+
+    }
+
+
+ 
+}
---
+++ ReflectionTask/ReflectionTask/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionTask
+{
+    internal class User
+    {
+        private int id;
+        private string name;
+        private string soyad;
+        private static int age;
+
+        public void GetFullName()
+        {
+            Console.WriteLine($"Full name: {name}  {soyad}");
+        }
+
+        public static void ChangeAge(int newAge)
+        {
+            age = newAge;
+        }
+    }
+}
