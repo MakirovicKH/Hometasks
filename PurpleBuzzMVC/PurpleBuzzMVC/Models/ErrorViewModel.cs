6e0f34417bd0cee5c8ea25c0e0e1eab9c6a4a8e5
MakirovicKH <mahirrx-ap215@code.edu.az>
2024-11-21 11:58:30 +0400

    PurpleBuzz MVC/

---
+++ PurpleBuzzMVC/PurpleBuzzMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PurpleBuzzMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
