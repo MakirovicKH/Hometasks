b939be683ad60c5e06df55d8d56b5bd121ec2c04
MakirovicKH <mahirrx-ap215@code.edu.az>
2024-11-23 11:39:41 +0400

    Purple Buzz AREA MVC Pr

---
+++ PurpleBuzzPr/PurpleBuzzPr/Areas/Admin/Controllers/WorkController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PurpleBuzzPr.DAL;
+using PurpleBuzzPr.Models;
+
+namespace PurpleBuzzPr.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WorkController : Controller
+    {
+        private readonly AppDbContext _context;
+        public WorkController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Work> works = await _context.Works.ToListAsync();
+
+            return View(works);
+        }
+    }
+}
